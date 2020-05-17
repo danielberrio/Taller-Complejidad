98b75dd9e07344d0a559063da91930ce25d76a31
danielberrio <45372077+danielberrio@users.noreply.github.com>
2020-05-16 22:22:18 -0500

    Correcciones a octal

--- octal.h
+++ octal.h
@@ -13,19 +13,19 @@ public:
     octal operator+(const octal &) const;
     octal operator-( octal &);
 private:
-    short dec[30];
+    short oct[30];
 
 };
 /*constructor predeterminado*/
 octal::octal(long valor)
 {
   for(int i = 0; i <= 29; i++)
-      dec[i] = 0;
+      oct[i] = 0;
 
   /*coloca los digitos del argumento dentro del arreglo*/
   for(int j = 29; valor != 0 && j >= 0; j--)
   {
-      dec[j] = valor % 8; //residuo del valor
+      oct[j] = valor % 8; //residuo del valor
       valor /= 8;
   }
 
@@ -37,10 +37,10 @@ octal octal::operator+(const octal &op2) const
     int acarreo = 0;
     for(int i = 29; i >= 0; i--)
     {
-        temp.dec[i] = dec[i] + op2.dec[i] - acarreo;
-        if(temp.dec[i] > 7)
+        temp.oct[i] = oct[i] + op2.oct[i] - acarreo;
+        if(temp.oct[i] > 7)
         {
-            temp.dec[i] %= 8; //reduce a 0-9
+            temp.oct[i] %= 8; //reduce a 0-9
             acarreo = 1;
         }
         else
@@ -52,25 +52,32 @@ octal octal::operator-(octal &op3)
 {
     octal temp;
     int acarreo = 0;
+
+
     for(int i = 29; i >= 0; i--)
     {
+        if(op3.oct[i] > (oct[i] + acarreo)){
 
-        //op3 es el segundo numero
-        if(op3.dec[i]>(dec[i] + acarreo)){
-            dec[i-1] -= 1;
-            acarreo = 8;
+             temp.oct[i] = op3.oct[i] - (oct[i] + acarreo);
+
         }
 
-        temp.dec[i] = (dec[i] + acarreo) - op3.dec[i];
+        if(oct[i] > (op3.oct[i] + acarreo)){
 
-        if(temp.dec[i] > 7)
+             temp.oct[i] = (oct[i] + acarreo) - op3.oct[i] ;
+
+        }
+        temp.oct[i] = (oct[i] + acarreo) - op3.oct[i] ;
+
+        if(temp.oct[i] > 7)
         {
-            temp.dec[i] %= 8; //reduce a 0-9
+            temp.oct[i] %= 8; //reduce a 0-7
             acarreo = 1;
         }
         else
             acarreo = 0;
     }
+
     return temp;
 }
 
@@ -78,14 +85,14 @@ octal octal::operator-(octal &op3)
 ostream& operator<<(ostream &salida, const octal &num)
 {
     int i;
-    for(i = 0; (num.dec[i] == 0) && (i <= 29); i++);
+    for(i = 0; (num.oct[i] == 0) && (i <= 29); i++);
     /*ignora ceros a la izquierda*/
 
     if(i == 30)
         salida << 0;
     else
         for( ; i <= 29; i++)
-            salida << num.dec[i];
+            salida << num.oct[i];
     return salida;
 }
 
